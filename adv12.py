9adf1cfa301dd472a2f317e2d231e5239030d96e
Niclas Änst <niclas.anst@gmail.com>
2018-12-30 19:51:08 +0100

    adv12 funkar på short, innan ändra för att testa riktiga

--- adv12.py
+++ adv12.py
@@ -4,28 +4,35 @@ test1 = "...##"
 # print(file_lines[1][0:5])
 # print(file_lines[1][9:10], end="")
 
-def generationizer(init_state_string):
-    if type(init_state_string) is not str:
-        temp_string = ""
-        for key in sorted(init_state_string.keys()):
-            temp_string += init_state_string[key]
-        init_state_string = temp_string
+def next_generation(state):
+    if type(state) is str:
+        pots = {}
+        for idx, ch in enumerate(state):
+            pots[idx] = ch
+    else:
+        pots = state
 
+    #for key in sorted(pots.keys()):
+        #print(key, pots[key])
 
+    first_key = None
+    last_key = None
+    for key in sorted(pots.keys()):
+        first_key = key if first_key == None else first_key
+        last_key = key if last_key == None or last_key < key else last_key
+
+    for a in range(last_key + 1, last_key + 6):
+        pots[a] = "."
+    for b in range(first_key - 1, first_key - 6, -1):
+        pots[b] = "."
+
+    #for key in sorted(pots.keys()):
+    #    print(key, pots[key])
+
+    return_dick = {}
     path = r"C:\Users\Herman\Desktop\PROGRAMMERING\Projekt\Advent_of_code\adv12-short.txt"
     with open(path) as file:
         file_lines = file.read().splitlines()
-
-    pots = {}
-    for idx, ch in enumerate(init_state_string):
-        pots[idx] = ch
-
-    for a in range(len(init_state_string),len(init_state_string)+5):
-        pots[a] = "."
-    for b in range(-1,-5,-1):
-        pots[b] = "."
-
-    return_dick = {}
     for key in sorted(pots.keys()):
         part_of_line = ""
         is_in = False
@@ -44,9 +51,53 @@ def generationizer(init_state_string):
             return_dick[key] = "."
     return return_dick
 
-gen1 = generationizer(init_state)
-gen2 = generationizer(gen1)
 
-for key2 in sorted(gen2.keys()):
-    if key2 == 0: print("0", end="")
-    print(gen2[key2], end="")
+
+g = next_generation(init_state)
+for _ in range(19):
+    g = next_generation(g)
+
+for key in sorted(g.keys()):
+    if key > -1:
+        print(g[key], end="")
+
+#     first_key = None
+#     last_key = None
+#     for key in sorted(pots.keys()):
+#         first_key = first_key or key
+#         last_key = last_key or key
+#         last_key = key if last_key < key else last_key
+#
+#     for a in range(last_key + 1, last_key + 6):
+#         pots[a] = "."
+#     for b in range(first_key - 1, first_key - 6, -1):
+#         pots[b] = "."
+#
+#
+#
+
+#
+#     return_dick = {}
+#     for key in sorted(pots.keys()):
+#         part_of_line = ""
+#         is_in = False
+#         if (key - 2) in pots and (key + 2) in pots:
+#             for k in range(key - 2, key + 3):
+#                 part_of_line += pots[k]
+#             for line in file_lines:
+#                 #print(key, part_of_line, line[0:5], " ", end="")
+#                 if part_of_line == line[0:5]:
+#                     #print("3","match", end="")
+#                     is_in = True
+#                 #print()
+#         if is_in:
+#             return_dick[key] = "#"
+#         else:
+#             return_dick[key] = "."
+#     return return_dick
+#
+# gen1 = generationizer(init_state)
+# gen2 = generationizer(gen1)
+#
+# for key2 in sorted(gen2.keys()):
+#     print(key2,gen2[key2], end="")
